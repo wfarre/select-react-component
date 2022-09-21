5d295da52b434a5c84355ccb5d342a4fed6efa82
william.farre@gmail.com <william.farre@gmail.com>
2022-09-21 16:02:12 +0800

    add jest test

--- src/Select.test.js
+++ src/Select.test.js
@@ -1,63 +1,82 @@
 import Select from "./lib/Select/Select";
-import {cleanup, fireEvent, render, screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom'
+import { act } from "react-test-renderer";
 
 
 
 describe("My select component", () => {
-    it("should display the array in the menu", () => {
+    it("should display the first element of the array in the button by default", () => {
         const array = ["apple", "banana", "strawberry"];
-        const handleClick = jest.fn();
 
         render(<Select data={array} />)
         const buttonText = screen.getByTestId("button-text")
-        console.log(buttonText.innerHTML);
         expect(buttonText.innerHTML).toEqual("apple");
 
         
-        const button = screen.getByTestId("button");
-        const dropdown = screen.getByTestId("dropdown");
+    })
+    describe("When I click on the button,", () => {
+        it("should display the dropdown menu", () => {
+            const array = ["apple", "banana", "strawberry"];
+            const handleClick = jest.fn();
+            window.HTMLElement.prototype.scrollIntoView = function() {};
 
-        expect(dropdown.classList).toContain("hide")
-        button.click();
-        expect(dropdown).toBeTruthy()
-        expect(dropdown.classList).not.toContain("hide")
+            render(<Select data={array} />)
 
-        // expect(dropdown).toContain("banana")
-
-
-        
-//         render( array.map((item, key = 0) => {
-//             key++;
-//             return (
-//               <li
-//                 className="dropdown__menu__item"
-//                 key={key}
-//                 onClick={handleClick}
-//                 item={item}
-//               >
-//                 <p className="dropdown__menu__item__content" data-testid = "content" >{item}</p>
-//               </li>)}))
-
-// // const items = screen.getAllByTestId("item");
-
-
-
-//         // items.map(item => {
-//         //    item.click()
-//         // })
-        
-
-//         expect(handleClick).toBeCalled()
-
-//         expect(buttonText.innerHTML).toEqual("banana");
-
-        // expect(screen.getByText("banana").).toBeInTheDocument();
-        // const items= screen.getAllByTestId()
-        
-        
-
-
+            const button = screen.getByTestId("button");
+            const dropdown = screen.getByTestId("dropdown");
+    
+            expect(dropdown.classList).toContain("hide")
+            expect(dropdown).not.toBeVisible()
+            expect(screen.getByText("banana")).not.toBeVisible()
+            fireEvent.click(button, handleClick)
+            expect(dropdown.classList).not.toContain("hide")
+            expect(dropdown).toBeVisible()
+            expect(screen.getByText("banana")).toBeVisible()
+        })
 
     })
+    describe("When I select an option,", () => {
+        it("should display the option is the button", async () => {
+            const array = ["apple", "banana", "strawberry"];
+            const handleClick = jest.fn();
+            window.HTMLElement.prototype.scrollIntoView = function() {};
+            const handleSelect = jest.fn()
+
+            render(<Select
+                data={array} 
+                value={array[0]}
+                handleSelect={(newValue) => handleSelect(newValue)} 
+            />)
+
+            const buttonText = screen.getByTestId("button-text")
+            expect(buttonText.innerHTML).toEqual("apple");
+
+            const button = screen.getByTestId("button");
+            const dropdown = screen.getByTestId("dropdown");
+
+    
+            fireEvent.click(button, handleClick)
+
+            expect(dropdown.classList).not.toContain("hide")
+            expect(dropdown).toBeVisible()
+            expect(screen.getByText("banana")).toBeVisible()
+
+            const bananaOption = screen.getByText("banana");
+           
+            act(()=> { 
+                bananaOption.dispatchEvent(new MouseEvent("click", {bubbles: true}))
+            })
+            expect(handleSelect).toBeCalled()
+            expect(bananaOption.innerHTML).toBe("banana")
+
+
+            expect(dropdown).not.toBeVisible()
+
+            expect(buttonText.innerHTML).toBe("banana")
+
+        })
+
+    })
+
 })
--- src/lib/Select/Select.jsx
+++ src/lib/Select/Select.jsx
@@ -8,8 +8,8 @@ const Select = ({ data, handleSelect }) => {
   const [selectedItem, setSelectedItem] = useState(data[0]);
 
   const handleOptionClicked = (e) => {
-    setSelectedItem(e.target.innerText);
-    handleSelect(e.target.innerText)
+    setSelectedItem(e.target.innerHTML);
+    handleSelect(e.target.innerHTML)
     setIsOpen(false)
   };
 
@@ -22,13 +22,13 @@ const Select = ({ data, handleSelect }) => {
   return (
     <div className="Select"  >
       <div className="dropdown" >
-        <div className={"dropdown__button"} data-bs-toggle="dropdown" aria-expanded={isOpen?"true":"false"} data-testid="button"   onClick={handleClick}  >
-          <p className="selected-item" data-testid="button-text">{selectedItem}</p>
+        <div className={"dropdown__button"} data-bs-toggle="dropdown" aria-expanded={isOpen} data-testid="button" onClick={handleClick}  >
+          <p className="selected-item" data-testid="button-text" value={selectedItem}>{selectedItem}</p>
           <span className="icon-wrapper">
             <Arrow className="arrow-icon"/>
           </span>
         </div>
-        <ul className={isOpen ? "dropdown__menu" : "dropdown__menu hide"} data-testid="dropdown" onMouseLeave={()=> setIsOpen(false)} >
+        <ul className={isOpen ? "dropdown__menu" : "dropdown__menu hide"} data-testid="dropdown" onMouseLeave={()=> setIsOpen(false)} hidden={!isOpen} >
           {data.map((item, key = 0) => {
             key++;
             return (
@@ -37,8 +37,9 @@ const Select = ({ data, handleSelect }) => {
                 key={key}
                 onClick={handleOptionClicked}
                 data-testid="item"
+                value={item}
               >
-                <p className="dropdown__menu__item__content" data-testid = "content" >{item}</p>
+                <p className="dropdown__menu__item__content" data-testid = "content" value={item}>{item}</p>
               </li>
             );
           })}
